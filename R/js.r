d32cb44ad07aac67f07a2a7e85c953101f6df8c0
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2017-07-15 07:46:09 +0200

    bugfix open.url

--- R/js.r
+++ R/js.r
@@ -116,7 +116,7 @@ callJS = function(.fun,..., .args=NULL, .app=getApp()) {
 open.url.from.app = function(url, token, new.tab=TRUE,target=if (new.tab) "_blank" else "", link.ui.id=NULL) {
   restore.point("open")
 
-  callJS(.fun = "window.open",list(url,target))
+  callJS(.fun = "window.open",.args=list(url,target))
   if (!is.null(link.ui.id)) {
     html = paste0('<a href="', url,'" class="button" target="',target,'">Click here if new window does not open automatically.</a>')
     setUI(link.ui.id, HTML(html))
